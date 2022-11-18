4b17bea2112041b248ef54c838c74c14d11df969
vishnu mudireddy <vishredd@gmail.com>
2022-11-18 08:08:55 +0530

    Remote Connection added

--- db/conn.php
+++ db/conn.php
@@ -1,8 +1,15 @@
 <?php
-    $host = 'localhost';
-    $db = 'attendance_db';
-    $user = 'root';
-    $pass = '';
+    //$host = 'localhost';
+    //$db = 'attendance_db';
+    //$user = 'root';
+    //$pass = '';
+    //$charset = 'utf8mb4';
+
+    //Remote Database Connection
+    $host = 'sql12.freesqldatabase.com';
+    $db = 'sql12578613';
+    $user = 'sql12578613';
+    $pass = 'rDbs53GN8b';
     $charset = 'utf8mb4';
 
     $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
